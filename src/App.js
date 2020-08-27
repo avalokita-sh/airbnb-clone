3db62833b703d179d35ab1a7bcc5462aa5cf4f7b
avalokitasharma <avalokitasharma@gmail.com>
2020-08-27 21:44:45 +0530

    added navbar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './Header.js';
+import Home from './Home.js';
+
+const App = () => {
+	return (
+		<div className="app">
+			<Header />
+			{/*Home*/}
+				<Home />
+				{/*header*/}
+
+				{/*banner*/}
+
+				{/*cards*/}
+
+				{/*footer*/}
+
+			{/*Search*/}
+		</div>
+	);
+}
+
+export default App;
